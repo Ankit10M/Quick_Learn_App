d36c5842ef8adef5fa13dbe7aa756843283e1654
Ankit <ankitbrijeshmishra10@gmail.com>
2025-10-21 13:57:31 +0530

    protected DB through env

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";    
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://ankitbrijeshmishra10_db_user:MishraQuiz-App@cluster0.9pjisye.mongodb.net/Quiz-App')
+    await mongoose.connect(process.env.MONGO_URI)
     .then(()=> console.log('MongoDB connected successfully'))
 }
